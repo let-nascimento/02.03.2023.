ad98b0d0200707a2f5f8d7f6e980b281e6494d2f
Letícia Nascimento Mathias <112521782+let-nascimento@users.noreply.github.com>
2023-03-02 16:54:43 -0300

    Add files via upload

---
+++ 02.03.2023/parametros.js
@@ -0,0 +1,7 @@
+function estaEntre(numero, minimo, maximo, inclusivo = false){
+    if(inclusivo == true){
+        return numero >= minimo && numero <= maximo;
+    }else{
+        return numero > minimo && numero <maximo;
+    }
+    }
---
+++ 02.03.2023/parimpar.js
@@ -0,0 +1,10 @@
+function parOuImpar (num){
+    //return num % 2 ? 'Impar' : 'par';
+
+    if( num % 2)
+        return 'par'
+        else
+        return 'impar'
+
+}
+console.log(parOuImpar(3))
